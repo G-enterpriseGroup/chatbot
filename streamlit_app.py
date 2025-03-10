a76881c2a01f3146fb6ceca380929ea6eb155592
G-enterpriseGroup <143067071+G-enterpriseGroup@users.noreply.github.com>
2025-03-10 01:50:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,6 @@
 import streamlit as st
 from openai import OpenAI
 
-# Add vector background image via custom CSS
-st.markdown(
-    """
-    <style>
-    body {
-        background-image: url("https://images.unsplash.com/photo-1562505299-a92c9a1c4f37?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.title("You should not be here, I will track your IP address, LEAVE!")
 
 # Set OpenAI API key from Streamlit secrets
