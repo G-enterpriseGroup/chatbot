670fc147c1cc639cc81b0cd0671305aca31d57bf
G-enterpriseGroup <143067071+G-enterpriseGroup@users.noreply.github.com>
2025-03-12 01:17:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-st.title("You should not be here, I will track your IP address, LEAVE!")
+st.title("🏎️💨")
 
 # Set OpenAI API key from Streamlit secrets
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
